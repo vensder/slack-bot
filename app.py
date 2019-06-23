e31caaee82602921b65664beb5d5b52f9d5c8b22
Dmitrii Makarov <dmitrii.makarov@propellerhead.co.nz>
2019-06-24 00:48:35 +1200

    added bot user id to condition

--- app.py
+++ app.py
@@ -8,10 +8,12 @@ with open('conf/conf.yaml', 'r') as conf_yaml:
 
 slack_token = bot_conf['slack']['slack_token']
 bot_id = bot_conf['slack']['bot_id']
+bot_user_id = ''
 
 
 @slack.RTMClient.run_on(event='hello')
 def hello_event(**payload):
+    global bot_user_id
     web_client = payload['web_client']
     bots_info = web_client.bots_info(bot=bot_id)
     print('hello event')
@@ -23,12 +25,13 @@ def hello_event(**payload):
 
 @slack.RTMClient.run_on(event='message')
 def say_hello(**payload):
+    global bot_user_id
     data = payload['data']
     print(data)
     print("="*80)
     web_client = payload['web_client']
     rtm_client = payload['rtm_client']
-    if 'text' in data and 'user' in data:
+    if ('text' in data) and ('user' in data) and (f"<@{bot_user_id}>" in data['text']):
         hello.hello(data, web_client)
 
 
