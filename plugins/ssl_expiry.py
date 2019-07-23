bd38de86e392bb1399071a14920d7faa0e53702a
Dmitrii Makarov <dmitrii.makarov@propellerhead.co.nz>
2019-07-23 16:50:20 +1200

    added openssl module

--- plugins/ssl_expiry.py
+++ plugins/ssl_expiry.py
@@ -2,9 +2,16 @@ import socket
 import ssl
 import datetime
 import re
-
+import OpenSSL                                                                                                                                              
 
 trigger_phrase = '@ssl date'
+
+
+def get_ssl_expiry_datetime_v2(hostname):
+    ssl_date_fmt = r'%Y%m%d%H%M%SZ'        
+    cert = ssl.get_server_certificate((hostname, 443))
+    x509 = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, cert)
+    return datetime.datetime.strptime(x509.get_notAfter().decode("utf-8"), ssl_date_fmt)    
 
 
 def get_ssl_expiry_datetime(hostname):
@@ -23,9 +30,6 @@ def get_ssl_expiry_datetime(hostname):
     # parse the string from the certificate into a Python datetime object
     return datetime.datetime.strptime(ssl_info['notAfter'], ssl_date_fmt) # 2016-04-19 23:59:59 # <class 'datetime.datetime'>
 
-# emoji
-# :white_check_mark:
-# :warning:
 
 def ssl_expiry(data, web_client, bot_user_id):
     print('started ssl expiry')
@@ -74,3 +78,30 @@ def ssl_expiry(data, web_client, bot_user_id):
                 print('network: ', domain)
                 
             
+                try:
+                    expiry_datetime = get_ssl_expiry_datetime_v2(domain)
+                    now = datetime.datetime.now()
+                    delta = expiry_datetime - now
+                    
+                    emoji = ':white_check_mark:'
+                    if delta.days < 30:
+                        emoji = ':warning:'
+                    output_string = emoji + 'Certificate for domain ' + domain + ' will expire in ' + str(delta.days) + ' days (' + str(expiry_datetime) + ')'
+                    print(output_string)
+
+                    web_client.chat_postMessage(
+                        channel=channel_id,
+                        text=output_string,
+                        thread_ts=thread_ts
+                    )
+                    
+                except Exception as e:
+                    emoji = ':exclamation:'
+                    output_string = emoji + 'SSL checker' + str(e) + ': ' + domain
+                    web_client.chat_postMessage(
+                        channel=channel_id,
+                        text=output_string,
+                        thread_ts=thread_ts
+                    )                        
+                    print('Exception in ssl plugin: ',e)
+                    print('network: ', domain)
