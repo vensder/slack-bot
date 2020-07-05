5f3c30286a6db5496f91e181eaedb37af6857dde
Dmitrii Makarov <dmitrii.makarov@propellerhead.co.nz>
2020-07-05 22:09:00 +1200

    added bot icons,
    added template for ipcalc

--- plugins/ip_calc.py
+++ plugins/ip_calc.py
@@ -1,88 +1,54 @@
 import ipcalc
 
-except_phrase = 'Try something like this: ```@ipcalc 10.0.0.0/24```'
+except_phrase = 'Try something like this: ```!ipcalc 10.0.0.0/24```'
 outputs = []
+
+network_template = """
+```
+Network: {}/{}
+HostMin: {}
+HostMax: {}
+Netmask: {}
+NetSize: {}
+NetType: {}
+```
+"""
+
 
 def ip_calc(data, web_client, bot_user_id):
 
-    if '@ipcalc' in data['text'] and except_phrase not in data['text']:
+    if '!ipcalc' in data['text'] and except_phrase not in data['text']:
         channel_id = data['channel']
         thread_ts = data['ts']
         user = data['user']
-        output_string = ''
 
-        #network = ipcalc.Network('10.0.0.0/24')
+        emoji = ':heavy_check_mark:'
+
         print(data['text'])
-        network_string = data['text'].replace('@ipcalc', '').replace(f"<@{bot_user_id}>", '').strip() #text.lstrip('ipcalc ').strip('abcdefghijklmnopqrstuvwxyz ')
+        network_string = data['text'].replace(
+            '!ipcalc', '').replace(f"<@{bot_user_id}>", '').strip()
         print(network_string)
-        network = ipcalc.Network(network_string)
 
-        output_string += '```'
-        output_string += str(network.network()) + '/' + str(network.mask) + ' (network/mask)\n'
-        output_string += str(network.host_first()) + ' (first host)\n' # IP('172.16.42.1')
-        output_string += str(network.host_last()) + ' (last host)\n' # IP('172.16.42.2')
-        output_string += str(network.netmask()) + ' (netmask)\n' # 30
-        output_string += str(network.size()) + ' (size)\n'  # 4
-        output_string += str(network.info()) + ' (network type)\n' # 'PRIVATE'
-        output_string += '```'
+        try:
+            network = ipcalc.Network(network_string)
+            output_string = network_template.format(
+                str(network.network()), str(network.mask),
+                str(network.host_first()),
+                str(network.host_last()),
+                str(network.netmask()),
+                str(network.size()),
+                str(network.info())
+            )
 
+        except Exception as e:
+            emoji = ':exclamation:'
+            output_string = str(e)
+            print('Exception in ipcalculator: ', e)
+            print('network: ', network_string)
 
         web_client.chat_postMessage(
+            icon_emoji=emoji,
             channel=channel_id,
             text=output_string,
             thread_ts=thread_ts
         )
-
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-# import ipcalc
-
-# outputs = []
-
-# def process_message(data):
-    
-#     if 'text' in data and data['text']:
-#         try:
-#             text = data['text']
-#             channel = data['channel']
-#             except_phrase = 'Try something like this: ```@ipcalc 10.0.0.0/24```'
-
-#             if '@ipcalc' in text and except_phrase not in text:
-                
-#                 output_string = ''
-#                 emoji = ':spider_web:'
-#                 except_emoji = ':exclamation:'
-#                 bot_name = 'ip calculator'
-                
-#                 network = ipcalc.Network('10.0.0.0/24')
-#                 network_string = text.replace('@ipcalc', '').strip() #text.lstrip('ipcalc ').strip('abcdefghijklmnopqrstuvwxyz ')
-                
-#                 try:
-#                     network = ipcalc.Network(network_string)
-#                 except Exception as e:
-#                     outputs.append([channel, str(e) + '. ' + except_phrase, bot_name, except_emoji ])
-#                     print('Exception in ipcalculator: ', e)
-#                     print('network: ', network_string)
-                
-#                 try:
-#                     output_string += '```'
-#                     output_string += str(network.network()) + '/' + str(network.mask) + ' (network/mask)\n'
-#                     output_string += str(network.host_first()) + ' (first host)\n' # IP('172.16.42.1')
-#                     output_string += str(network.host_last()) + ' (last host)\n' # IP('172.16.42.2')
-#                     output_string += str(network.netmask()) + ' (netmask)\n' # 30
-#                     output_string += str(network.size()) + ' (size)\n'  # 4
-#                     output_string += str(network.info()) + ' (network type)\n' # 'PRIVATE'
-#                     output_string += '```'
-
-#                     outputs.append([channel, output_string, bot_name, emoji ])
-
-#                 except Exception as e:
-#                     outputs.append([channel, str(e) + '. ' + except_phrase, bot_name, except_emoji ])
-#                     print('Exception in ipcalculator: ',e)
-#                     print('network: ', network_string)
-
-
-#         except KeyError as e:
-#             print('KeyError Exception in ipcalculator.py: ', e)
-#             print(data)
--- plugins/ssl_expiry.py
+++ plugins/ssl_expiry.py
@@ -2,16 +2,16 @@ import socket
 import ssl
 import datetime
 import re
-import OpenSSL                                                                                                                                              
+import OpenSSL
 
-trigger_phrase = '@ssl date'
+trigger_phrase = '!ssl date'
 
 
 def get_ssl_expiry_datetime_v2(hostname):
-    ssl_date_fmt = r'%Y%m%d%H%M%SZ'        
+    ssl_date_fmt = r'%Y%m%d%H%M%SZ'
     cert = ssl.get_server_certificate((hostname, 443))
     x509 = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, cert)
-    return datetime.datetime.strptime(x509.get_notAfter().decode("utf-8"), ssl_date_fmt)    
+    return datetime.datetime.strptime(x509.get_notAfter().decode("utf-8"), ssl_date_fmt)
 
 
 def get_ssl_expiry_datetime(hostname):
@@ -22,18 +22,17 @@ def get_ssl_expiry_datetime(hostname):
         socket.socket(socket.AF_INET),
         server_hostname=hostname,
     )
-    # 3 second timeout
     conn.settimeout(3.0)
 
     conn.connect((hostname, 443))
     ssl_info = conn.getpeercert()
     # parse the string from the certificate into a Python datetime object
-    return datetime.datetime.strptime(ssl_info['notAfter'], ssl_date_fmt) # 2016-04-19 23:59:59 # <class 'datetime.datetime'>
+    # 2016-04-19 23:59:59 # <class 'datetime.datetime'>
+    return datetime.datetime.strptime(ssl_info['notAfter'], ssl_date_fmt)
 
 
 def ssl_expiry(data, web_client, bot_user_id):
-    print('started ssl expiry')
-    
+
     if trigger_phrase in data['text']:
         channel_id = data['channel']
         thread_ts = data['ts']
@@ -41,67 +40,72 @@ def ssl_expiry(data, web_client, bot_user_id):
         output_string = ''
 
         text = data['text']
-        
-        mask = re.compile(r'([A-Za-z0-9-]+\.)+\w+') # domain pattern
+        emoji = ':heavy_check_mark:'
+
+        mask = re.compile(r'([A-Za-z0-9-]+\.)+\w+')  # domain pattern
         if mask.search(text):
             search_domain = mask.search(text)
             domain = search_domain.group()
-            
-            print(domain); print(type(domain))
-            
+
             try:
                 expiry_datetime = get_ssl_expiry_datetime(domain)
                 now = datetime.datetime.now()
                 delta = expiry_datetime - now
-                
-                emoji = ':white_check_mark:'
+
                 if delta.days < 30:
                     emoji = ':warning:'
-                output_string = emoji + 'Certificate for domain ' + domain + ' will expire in ' + str(delta.days) + ' days (' + str(expiry_datetime) + ')'
+                output_string = 'Certificate for domain ' + domain + \
+                    ' will expire in ' + \
+                    str(delta.days) + ' days (' + str(expiry_datetime) + ')'
                 print(output_string)
 
                 web_client.chat_postMessage(
+                    icon_emoji=emoji,
                     channel=channel_id,
                     text=output_string,
                     thread_ts=thread_ts
                 )
-                
+
             except Exception as e:
                 emoji = ':exclamation:'
-                output_string = emoji + 'SSL checker' + str(e) + ': ' + domain
+                output_string = 'SSL checker' + str(e) + ': ' + domain
                 web_client.chat_postMessage(
+                    icon_emoji=emoji,
                     channel=channel_id,
                     text=output_string,
                     thread_ts=thread_ts
-                )                        
-                print('Exception in ssl plugin: ',e)
+                )
+                print('Exception in ssl plugin: ', e)
                 print('network: ', domain)
-                
-            
+
                 try:
                     expiry_datetime = get_ssl_expiry_datetime_v2(domain)
                     now = datetime.datetime.now()
                     delta = expiry_datetime - now
-                    
-                    emoji = ':white_check_mark:'
+
                     if delta.days < 30:
                         emoji = ':warning:'
-                    output_string = emoji + 'Certificate for domain ' + domain + ' will expire in ' + str(delta.days) + ' days (' + str(expiry_datetime) + ')'
+                    output_string = 'Certificate for domain ' + domain + \
+                        ' will expire in ' + \
+                        str(delta.days) + ' days (' + str(expiry_datetime) + ')'
                     print(output_string)
 
                     web_client.chat_postMessage(
+                        icon_emoji=emoji,
                         channel=channel_id,
                         text=output_string,
                         thread_ts=thread_ts
                     )
-                    
+
                 except Exception as e:
                     emoji = ':exclamation:'
-                    output_string = emoji + 'SSL checker' + str(e) + ': ' + domain
+                    output_string = 'SSL checker' + \
+                        str(e) + ': ' + domain
                     web_client.chat_postMessage(
+                        icon_emoji=":warning:",
                         channel=channel_id,
                         text=output_string,
                         thread_ts=thread_ts
-                    )                        
-                    print('Exception in ssl plugin: ',e)
+                    )
+                    print('Exception in ssl plugin: ', e)
                     print('network: ', domain)
